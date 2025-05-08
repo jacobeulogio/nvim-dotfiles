95f1710f4c9448771f3c73eb3c915c13aa5c51c8
jacobeulogio <benitezjacob35@gmail.com>
2025-05-08 22:12:16 +0800

    yazi

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,6 +1,5 @@
 return {
 
-  { 'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons' },
   { 'akinsho/toggleterm.nvim', version = '*', config = true },
 
   -- {
@@ -36,6 +35,48 @@ return {
   --     window_overlap_clear_ft_ignore = { 'cmp_menu', 'cmp_docs', '' },
   --   },
   -- },
+  {
+    'rolv-apneseth/tfm.nvim',
+    lazy = false,
+    opts = {
+      file_manager = 'yazi',
+      -- Replace netrw entirely
+      -- Default: false
+      replace_netrw = true,
+      -- Enable creation of commands
+      -- Default: false
+      -- Commands:
+      --   Tfm: selected file(s) will be opened in the current window
+      --   TfmSplit: selected file(s) will be opened in a horizontal split
+      --   TfmVsplit: selected file(s) will be opened in a vertical split
+      --   TfmTabedit: selected file(s) will be opened in a new tab page
+      enable_cmds = false,
+      -- Custom keybindings only applied within the TFM buffer
+      -- Default: {}
+      keybindings = {
+        ['<ESC>'] = 'q',
+        -- Override the open mode (i.e. vertical/horizontal split, new tab)
+        -- Tip: you can add an extra `<CR>` to the end of these to immediately open the selected file(s) (assuming the TFM uses `enter` to finalise selection)
+        ['<C-v>'] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.vsplit)<CR>",
+        ['<C-x>'] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.split)<CR>",
+        ['<C-t>'] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.tabedit)<CR>",
+      },
+      -- Customise UI. The below options are the default
+      ui = {
+        border = 'rounded',
+        height = 1,
+        width = 1,
+        x = 0.5,
+        y = 0.5,
+      },
+    },
+    keys = {
+      { '<leader>ee', ':Tfm<CR>', desc = 'TFM' },
+      -- { "<leader>eh", ":TfmSplit<CR>", desc = "TFM - horizontal split", },
+      -- { "<leader>ev", ":TfmVsplit<CR>", desc = "TFM - vertical split", },
+      -- { "<leader>et", ":TfmTabedit<CR>", desc = "TFM - new tab", },
+    },
+  },
 
   {
     'rmagatti/auto-session',
