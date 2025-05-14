7c63fa7f7b7fa06a81da356883704c5d7a352f41
jacobbenitez <jacob.benitez@triumphhomedepot.com>
2025-05-14 10:53:22 +0800

    update lsp.lua

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,5 +1,16 @@
 return {
   {
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+
+  {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
