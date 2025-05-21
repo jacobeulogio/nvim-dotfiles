f97792dae5ee0d2ae048a4ea287159902cb739f7
jacobbenitez <jacob.benitez@triumphhomedepot.com>
2025-05-21 14:13:32 +0800

    Updates

--- lua/options.lua
+++ lua/options.lua
@@ -65,15 +65,4 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- Tmux Window Names
-local uv = vim.uv
-
-vim.api.nvim_create_autocmd({ 'VimEnter', 'VimLeave' }, {
-  callback = function()
-    if vim.env.TMUX_PLUGIN_MANAGER_PATH then
-      uv.spawn(vim.env.TMUX_PLUGIN_MANAGER_PATH .. '/tmux-window-name/scripts/rename_session_windows.py', {})
-    end
-  end,
-})
-
 vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
