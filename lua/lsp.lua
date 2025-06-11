2dcf98b2a00241bb22ed71c3469b8ca164e79990
jacobeulogio <benitezjacob35@gmail.com>
2025-06-11 11:29:58 +0800

    Nix

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -84,34 +84,6 @@ return {
         end,
       })
 
-      -- Diagnostic Config     See :help vim.diagnostic.Opts
-      -- vim.diagnostic.config {
-      --   severity_sort = true,
-      --   float = { border = 'rounded', source = 'if_many' },
-      --   underline = { severity = vim.diagnostic.severity.ERROR },
-      --   signs = vim.g.have_nerd_font and {
-      --     text = {
-      --       [vim.diagnostic.severity.ERROR] = '󰅚 ',
-      --       [vim.diagnostic.severity.WARN] = '󰀪 ',
-      --       [vim.diagnostic.severity.INFO] = '󰋽 ',
-      --       [vim.diagnostic.severity.HINT] = '󰌶 ',
-      --     },
-      --   } or {},
-      --   virtual_text = {
-      --     source = 'if_many',
-      --     spacing = 2,
-      --     format = function(diagnostic)
-      --       local diagnostic_message = {
-      --         [vim.diagnostic.severity.ERROR] = diagnostic.message,
-      --         [vim.diagnostic.severity.WARN] = diagnostic.message,
-      --         [vim.diagnostic.severity.INFO] = diagnostic.message,
-      --         [vim.diagnostic.severity.HINT] = diagnostic.message,
-      --       }
-      --       return diagnostic_message[diagnostic.severity]
-      --     end,
-      --   },
-      -- }
-
       vim.diagnostic.config {
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
@@ -158,7 +130,10 @@ return {
                 fixable = { 'ALL' },
                 unfixable = {
                   'B',
-                  'F401', -- Imported but unused 'E501', 'UP032', 'RUF100',
+                  'F401', -- Imported but unused 
+                  'E501', 
+                  'UP032', 
+                  'RUF100',
                 },
               },
             },
@@ -189,6 +164,7 @@ return {
             },
           },
         },
+
         lua_ls = {
           settings = {
             Lua = {
@@ -260,6 +236,7 @@ return {
           'ruff_fix',
           'ruff_format',
         },
+        nix = { 'alejandra' }
       },
     },
   },
