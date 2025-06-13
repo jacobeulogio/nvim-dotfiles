2f19a905d73dd494736d985e92557cd3af7f8ac1
jacobbenitez <eulogio@localhost.localdomain>
2025-06-13 08:38:03 +0800

    DBUI

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -43,6 +43,13 @@ return {
     },
     init = function()
       vim.g.db_ui_use_nerd_fonts = 1
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'dbui', 'sql', 'mysql', 'plsql' },
+        callback = function()
+          vim.keymap.set({ 'n', 'v' }, '<leader><CR>', '<Plug>(DBUI_ExecuteQuery)', { buffer = true, silent = true, desc = 'Execute sql query' })
+        end,
+      })
     end,
   },
 
