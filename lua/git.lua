d7f2a78c6ad1dcc05109f7c35af870ccd34e3aae
jacobbenitez <eulogio@localhost.localdomain>
2025-06-24 16:28:12 +0800

    Updates

--- lua/git.lua
+++ lua/git.lua
@@ -1,7 +1,6 @@
 return {
   'tpope/vim-fugitive',
-  config = function()
-    vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+  config = function() vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
 
     local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup('ThePrimeagen_Fugitive', {})
 
