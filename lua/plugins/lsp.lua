648c3fd88612f7d8ebaac0d5ffcf28115626e07e
jacobeulogio <benitezjacob35@gmail.com>
2025-08-05 01:17:39 +0800

    lsp removed mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,7 +4,6 @@ return {
     ft = 'lua',
     opts = {
       library = {
-        -- Load luvit types when the `vim.uv` word is found
         { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
       },
     },
@@ -12,9 +11,9 @@ return {
   {
     'neovim/nvim-lspconfig',
     dependencies = {
-      { 'mason-org/mason.nvim', opts = {} },
-      { 'mason-org/mason-lspconfig.nvim', opts = {} },
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      -- { 'mason-org/mason.nvim', opts = {} },
+      -- { 'mason-org/mason-lspconfig.nvim', opts = {} },
+      -- 'WhoIsSethDaniel/mason-tool-installer.nvim',
       { 'j-hui/fidget.nvim', opts = {} },
       'saghen/blink.cmp',
     },
@@ -37,50 +36,6 @@ return {
           map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
-          -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-          ---@param client vim.lsp.Client
-          ---@param method vim.lsp.protocol.Method
-          ---@param bufnr? integer some lsp support methods only in specific files
-          ---@return boolean
-          local function client_supports_method(client, method, bufnr)
-            if vim.fn.has 'nvim-0.11' == 1 then
-              return client:supports_method(method, bufnr)
-            else
-              return client.supports_method(method, { bufnr = bufnr })
-            end
-          end
-
-          -- Reference Highlighting
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.clear_references,
-            })
-
-            vim.api.nvim_create_autocmd('LspDetach', {
-              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-              callback = function(event2)
-                vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-              end,
-            })
-          end
-
-          -- Inlay Hints
-          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[T]oggle Inlay [H]ints')
-          end
         end,
       })
 
@@ -109,34 +64,12 @@ return {
         },
       }
 
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-
       local servers = {
         ruff = {
           init_options = {
             settings = {
               lineLength = 120,
               logLevel = 'debug',
-              -- lint = {
-              --   select = { 'ALL' },
-              --   ignore = {
-              --     'D',
-              --     'W',
-              --     'ANN',
-              --     'INP001',
-              --     'PD901',
-              --     'ERA001',
-              --     -- 'Q',
-              --   },
-              --   fixable = { 'ALL' },
-              --   unfixable = {
-              --     'B',
-              --     'F401', -- Imported but unused
-              --     'E501',
-              --     'UP032',
-              --     'RUF100',
-              --   },
-              -- },
             },
           },
         },
@@ -174,36 +107,29 @@ return {
             },
           },
         },
+
       gopls = {},
       prettier = {},
       rust_analyzer = {},
       alejandra = {},
+      nil_ls = {},
 
       }
 
-
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua',
-        'markdownlint',
-      })
-
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      ---@type MasonLspconfigSettings
-      ---@diagnostic disable-next-line: missing-fields
-      require('mason-lspconfig').setup {
-        automatic_enable = vim.tbl_keys(servers or {}),
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            -- require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
+      -- ---@type MasonLspconfigSettings
+      -- ---@diagnostic disable-next-line: missing-fields
+      -- require('mason-lspconfig').setup {
+      --   automatic_enable = vim.tbl_keys(servers or {}),
+      --   handlers = {
+      --     function(server_name)
+      --       local server = servers[server_name] or {}
+      --       server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+      --     end,
+      --   },
+      -- }
 
       for server_name, config in pairs(servers) do
+        vim.lsp.enable(server_name)
         vim.lsp.config(server_name, config)
       end
     end,
