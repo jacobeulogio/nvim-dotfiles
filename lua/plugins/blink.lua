cd83b1c471f6f489ace78256b38b81fd4eff92c8
Jacob Benitez <jacob.benitez@triumphhomedepot.com>
2025-09-08 14:38:57 +0800

    typr

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -30,9 +30,7 @@ return {
     keymap = {
       preset = 'default',
     },
-    appearance = {
-      nerd_font_variant = 'mono',
-    },
+    appearance = { nerd_font_variant = 'mono', },
     completion = {
       documentation = { auto_show = true, auto_show_delay_ms = 500 },
       menu = {
